a3aa7477193d31d742d58036ce4db9a45a26d2fa
Pablo Caro <pcaro@users.noreply.github.com>
2013-06-20 08:45:07 +0200

    New computer resulution

--- dos_pantallas.sh
+++ dos_pantallas.sh
@@ -2,6 +2,6 @@
 INT="LVDS1"
 EXT="VGA1"
 xrandr --output $EXT --mode 1280x1024;
-xrandr --output $INT --mode 1280x800;
+xrandr --output $INT --mode 1366x768;
 xrandr --output $EXT --above $INT
 xrandr --output $INT --primary
